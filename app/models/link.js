692495f3b2621df83d9f8352555064ab4d8c0318
Hack Reactor Students <accounts@hackreactor.com>
2015-01-15 16:17:03 -0800

    Refactored to mongoDb on our local server

--- app/models/link.js
+++ app/models/link.js
@@ -1,3 +1,46 @@
+var db = require('../config');
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
+var crypto = require('crypto');
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+// define schema
+var linkSchema = new Schema({
+  linkID: ObjectId,
+  url: String,
+  base_url: String,
+  code: String,
+  title: String,
+  visits: Number
+});
+
+linkSchema.methods.cryptoSomething = function(callback, attrs, options) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.get('url'));
+  this.set('code', shasum.digest('hex').slice(0, 5));
+  callback();
+}
+
+var Link = db.model('Link', linkSchema);
+
+//before we save the link, we crypto that
+linkSchema.pre('save', function(next) {
+  this.cryptoSomething(next);
+});
+
+var cnn = new Link({url:'http:www.cnn.com'});
+console.log(cnn);
+// cnn.save();
+
+module.exports = Link;
+
+
+
+
+
 /*var db = require('../config');
 var crypto = require('crypto');
 
--- app/models/user.js
+++ app/models/user.js
@@ -16,57 +16,25 @@ var userSchema = new Schema({
 
 // assign function to the 'methods' object of our schema
 userSchema.methods.hashPassword = function(){
-  console.log('inside of hashPassword');
   var cipher = Promise.promisify(bcrypt.hash);
   // COME BACK: check on syntax of getting
   return cipher(this.get('password'), null, null).bind(this)
     .then(function(hash) {
-      console.log('inside of hashing', hash);
       this.set('password', hash);
-      console.log(this);
     });
 };
 
+userSchema.methods.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+    callback(isMatch);
+  });
+}
+
 var User = db.model('User', userSchema);
 
+//before we save the user, we hash their password.
 userSchema.pre('save', function(next) {
-  console.log('this inside of pre');
-  console.log(this);
-
   this.hashPassword().then(next);
 });
-
-var omar = new User({username: 'omar', password: 'yo'});
-// omar.hashPassword();
-
-// save model to MongoDB
-omar.save(function(err){
-  if (err){
-    console.log(err);
-  } else {
-    console.log('omar is safe!')
-  }
-});
-console.log(omar);
-
-/*var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
-});*/
 
 module.exports = User;
